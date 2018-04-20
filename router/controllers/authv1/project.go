67ab177378a13d0083dde5b024add47e049f5252
hujiahui <hujiahuigaffey_i@didichuxing.com>
2018-04-20 22:29:22 +0800

    delete error annotations

--- router/controllers/authv1/project.go
+++ router/controllers/authv1/project.go
@@ -4,9 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func RegisterProject(router *gin.RouterGroup) {
 
-	router.POST("project", httpHandlerAddProject)   //用户创建工程（项目所有成员）
-	router.DELETE("project", httpHandlerDelProject) //用户删除工程（项目所有成员）
-	router.PUT("project", httpHandlerUpdateProject) //用户编辑工程（项目所有成员）
+	router.POST("project", httpHandlerAddProject)
+	router.DELETE("project", httpHandlerDelProject)
+	router.PUT("project", httpHandlerUpdateProject)
 	router.GET("project", httpHandlerConstruct)
 }
 
@@ -14,11 +14,7 @@ func httpHandlerAddProject(c *gin.Context) {
 
 }
 
-//删除工程(只有创建者可以删除，将删除标志重置）
 func httpHandlerDelProject(c *gin.Context) {
-	userId, _ := c.Get("userId")
-	//获取projectId
-	//判断userId是否是创建者
 
 }
 
